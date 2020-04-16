f117b49305581dae17d3fab220d2b4f391ccdf20
Dongmin <kdm0228@gmail.com>
2020-04-16 11:02:44 +0900

    python3에서도 실행가능하도록 수정 - data send/recv 부분 수정 (endoce/decode) - thread를 이용한 데이터 전송/수신

--- insoft/chat/client.py
+++ insoft/chat/client.py
@@ -2,53 +2,92 @@
 
 import sys
 import socket
-import select
+import threading
  
 def chat_client():
+    
+    # 파라미터로 hostname port를 받아서 처리한다.
     if(len(sys.argv) < 3) :
-        print ('Usage : python chat_client.py hostname port')
+        print ('Usage : python client.py hostname port')
         sys.exit()
 
     host = sys.argv[1]
     port = int(sys.argv[2])
-     
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(2)
-     
-    # connect to remote host
+    
+    # 소켓을 연결한다 (IPv4, TCP)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(2000)
+    
     try :
-        s.connect((host, port))
+        sock.connect((host, port))
     except :
         print ('Unable to connect')
         sys.exit()
      
     print ('Connected to remote host. You can start sending messages')
-    sys.stdout.write('[Me] '); sys.stdout.flush()
-     
-    while 1:
-        socket_list = [sys.stdin, s]
-         
-        # Get the list sockets which are readable
-        ready_to_read,ready_to_write,in_error = select.select(socket_list , [], [])
-         
-        for sock in ready_to_read:             
-            if sock == s:
-                # incoming message from remote server, s
-                data = sock.recv(4096)
-                if not data :
-                    print ('\nDisconnected from chat server')
-                    sys.exit()
-                else :
-                    #print data
-                    sys.stdout.write(data)
-                    sys.stdout.write('[Me] '); sys.stdout.flush()     
-            
+    
+    # 데이터를 수신할 스레드 생성
+    receiver = threading.Thread(target=recvThread, args=(sock,)) 
+    receiver.start()
+
+    # 데이터를 전송할 스레드 생성
+    sender = threading.Thread(target=sendThread, args=(sock,)) 
+    sender.start()
+    
+# 전송 스레드    
+def sendThread(sock):
+
+    print('\n[SENDER] Thread start')
+
+    while True:
+
+        try:
+            sys.stdout.write('[Me] '); sys.stdout.flush()    
+
+            # 데이터 입력을 받는다
+            msg = sys.stdin.readline()
+
+            if msg == 'quit\n':
+                break
+
+            # 받은 데이터(str)를 byte로 변환하여 데이터 전송
+            sock.send(msg.encode())
+        except Exception as e:
+             print('\n[SENDER] Unable to connect ', e)
+             break
+
+    sock.close() 
+
+
+
+# 수신 스레드
+def recvThread(sock): 
+    print('\n[RECEIVER] Thread start')
+    
+    while True: 
+
+        try:
+
+            # 서버로 부터 전달된 데이터 를 받는다
+            data = sock.recv(4096)
+                        
+            if not data: 
+                # 데이터가 아닐경우 연결종료
+                print('\n[RECEIVER] Disconnected from chat server')
+                sys.exit()
             else :
-                # user entered a message
-                msg = sys.stdin.readline()
-                s.send(msg)
-                sys.stdout.write('[Me] '); sys.stdout.flush() 
+                # 수신된 데이터(byte)를 변환(str)한다.
+                decodeData = data.decode()
+
+                # 변환된 데이터 출력
+                sys.stdout.write(decodeData)
+                sys.stdout.write('[Me] '); sys.stdout.flush()    
+            
+        except Exception as e:
+            print('\n[RECEIVER] Disconnected from chat server ', e)
+            break
+
+    sock.close() 
 
 if __name__ == "__main__":
-
     sys.exit(chat_client())
--- insoft/chat/server.py
+++ insoft/chat/server.py
@@ -9,46 +9,67 @@ PORT = 9900
 
 def chat_server():
 
+    # 소켓 객체를 생성합니다. 
+    # 주소 체계(address family)로 IPv4, 소켓 타입으로 TCP 사용합니다.  
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    # 포트 사용중이라 연결할 수 없다는 
+    # WinError 10048 에러 해결를 위해 필요합니다. 
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    # bind 함수는 소켓을 특정 네트워크 인터페이스와 포트 번호에 연결하는데 사용됩니다.
+    # HOST는 hostname, ip address, 빈 문자열 ""이 될 수 있습니다.
+    # 빈 문자열이면 모든 네트워크 인터페이스로부터의 접속을 허용합니다. 
+    # PORT는 1-65535 사이의 숫자를 사용할 수 있습니다.
     server_socket.bind((HOST, PORT))
-    server_socket.listen(10)
+
+    # 서버가 클라이언트의 접속을 허용하도록 합니다. 
+    server_socket.listen()
  
-    # add server socket object to the list of readable connections
+    # SELECTOR를 사용하기 위해 서버소켓을 배열에 추가
     SOCKET_LIST.append(server_socket)
  
     print ("Chat server started on port " + str(PORT))
  
     while 1:
 
-        # get the list sockets which are ready to be read through select
-        # 4th arg, time_out  = 0 : poll and never block
-        ready_to_read,ready_to_write,in_error = select.select(SOCKET_LIST,[],[],0)
+        # SELECTOR를 이용하여 소켓들의 상태를 파악하여 대기함
+        ready_to_read,ready_to_write,in_error = select.select(SOCKET_LIST, [], [], 0)
       
+        # 데이터가 준비된 소켓에서 데이터를 읽어들인다.
         for sock in ready_to_read:
-            # a new connection request recieved
+            
+            # 서버소켓일경우 클라이언트 접속 처리
             if sock == server_socket: 
                 sockfd, addr = server_socket.accept()
+
+                # 접속된 클라이언트 소켓은 소켓배열에 추가한다.
                 SOCKET_LIST.append(sockfd)
                 print ("Client (%s, %s) connected" % addr)
                  
+                # 클라이언트가 추가된 사항을 접속된 전체 클라이언트에게 전달한다
                 broadcast(server_socket, sockfd, '[%s:%s] entered our chatting room\n' % addr)
              
-            # a message from a client, not a new connection
+            # 클라이언트로 부터 전달받은 경우
             else:
-                # process data recieved from client, 
+                
                 try:
-                    # receiving data from the socket.
+                    # 수신받은 데이터를 읽어들인다.
                     data = sock.recv(RECV_BUFFER)
+
                     if data:
-                        # there is something in the socket
-                        broadcast(server_socket, sock, "\r" + '[' + str(sock.getpeername()) + '] ' + data)  
+                        # 수신데이터 (bytes)를 변환 (str) 한다.
+                        decodeData = data.decode()
+                        print("\r" + '[' + str(sock.getpeername()) + '] ' + decodeData)
+
+                        # 수신된 데이터를 접속된 전체 클라이언트에게 전달한다
+                        broadcast(server_socket, sock, "\r" + '[' + str(sock.getpeername()) + '] ' + decodeData)  
                     else:
-                        # remove the socket that's broken    
+                        # 데이터가 아닐경우 해당 소켓을 제외한다. 
                         if sock in SOCKET_LIST:
                             SOCKET_LIST.remove(sock)
 
-                        # at this stage, no data means probably the connection has been broken
+                        # 제외된 클라이언트 정보를 전체 클라이언트에게 전달한다.
                         broadcast(server_socket, sock, "Client (%s, %s) is offline\n" % addr) 
 
                 # exception 
@@ -65,7 +86,9 @@ def broadcast (server_socket, sock, message):
         if socket != server_socket and socket != sock :
             try :
                 print(message)
-                socket.send(message)
+
+                # 전달할 데이터(str)를 변환(bytes) 한다.
+                socket.send(message.encode())
             except :
                 # broken socket connection
                 socket.close()
